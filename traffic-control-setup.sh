ab7a8c9cd97cee5860766bf163d8b3c5eba580e3
Lucas Roman <lucas.roman@student.uclouvain.be>
2022-11-01 18:31:04 +0100

    custom tc script ok

--- traffic-control-setup.sh
+++ traffic-control-setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+
+# Usage : sudo ./traffic-control-setup.sh [interface] [destination ip] [destination port] [source port]
+
 TC=/sbin/tc
 
-IF=lo
+IF=$1
 
-LIMIT=100mbit
+LIMIT=1mbit
 
-DST_CIDR=127.0.0.1/32
+DST_IP=$2
+DST_PORT=$3
+SRC_PORT=$4
 
 U32="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
 
 create () {
-  echo "== SHAPING INIT =="
 
   $TC qdisc add dev $IF root handle 1:0 htb \
     default 30
@@ -19,16 +23,18 @@ create () {
   $TC class add dev $IF parent 1:0 classid \
     1:1 htb rate $LIMIT
 
-  $U32  match ip dst $DST_CIDR flowid 1:1
+  $U32 \
+    match ip dst $DST_IP \
+    match ip dport $DST_PORT 0xffff \
+    match ip sport $SRC_PORT 0xffff \
+    flowid 1:1
 
-  echo "== SHAPING DONE =="
 }
 
 clean () {
-  echo "== CLEAN INIT =="
-  $TC qdisc del dev $IF root
-  echo "== CLEAN DONE =="
+  $TC qdisc del dev $IF root 2>/dev/null
 }
 
 clean
 create
+echo "Done !"
