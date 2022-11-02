3ab5a8403e1723d72d410a5be52349d8e68852ab
Lucas Roman <lucas.roman@student.uclouvain.be>
2022-11-02 15:22:25 +0100

    fixed tc

--- traffic-control-setup.sh
+++ traffic-control-setup.sh
@@ -7,7 +7,7 @@ TC=/sbin/tc
 
 IF=$1
 
-LIMIT=1mbit
+LIMIT=1kbit
 
 DST_IP=$2
 DST_PORT=$3
@@ -16,13 +16,17 @@ SRC_PORT=$4
 U32="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
 
 create () {
+  SHOW=$(tc qdisc show dev "$IF")
+  EMPTY="qdisc noqueue 0: root refcnt 2"
 
-  $TC qdisc add dev $IF root handle 1:0 htb \
-    default 30
+  if  [[ $SHOW == "$EMPTY"* ]] ; then
+    echo "No qdisc on "$IF" : creating one ..."
+    $TC qdisc add dev $IF root handle 1:0 htb \
+      default 30
 
-  $TC class add dev $IF parent 1:0 classid \
-    1:1 htb rate $LIMIT
-
+    $TC class add dev $IF parent 1:0 classid \
+      1:1 htb rate $LIMIT
+  fi
   $U32 \
     match ip dst $DST_IP \
     match ip dport $DST_PORT 0xffff \
@@ -31,10 +35,10 @@ create () {
 
 }
 
-clean () {
-  $TC qdisc del dev $IF root 2>/dev/null
-}
+# clean () {
+#   $TC qdisc del dev $IF root 2>/dev/null
+# }
 
-clean
+# clean
 create
 echo "Done !"
