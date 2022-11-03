21cc1317e7ee211baae8bcad2142d97b34f9d813
Lucas Roman <lucas.roman@student.uclouvain.be>
2022-11-03 10:03:42 +0100

    Fixed ports on tc script

--- traffic-control-setup.sh
+++ traffic-control-setup.sh
@@ -7,7 +7,7 @@ TC=/sbin/tc
 
 IF=$1
 
-LIMIT=1kbit
+LIMIT=50mbit # Il faut trouver une solution pour le doser un peu mieux
 
 DST_IP=$2
 DST_PORT=$3
@@ -17,20 +17,20 @@ U32="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
 
 create () {
   SHOW=$(tc qdisc show dev "$IF")
-  EMPTY="qdisc noqueue 0: root refcnt 2"
+  EMPTY="qdisc pfifo_fast 0: root refcnt 2 bands 3 priomap 1 2 2 2 1 2 0 0 1 1 1 1 1 1 1 1"
 
   if  [[ $SHOW == "$EMPTY"* ]] ; then
     echo "No qdisc on "$IF" : creating one ..."
-    $TC qdisc add dev $IF root handle 1:0 htb \
+    $TC qdisc replace dev $IF root handle 1:0 htb \
       default 30
 
-    $TC class add dev $IF parent 1:0 classid \
+    $TC class replace dev $IF parent 1:0 classid \
       1:1 htb rate $LIMIT
   fi
   $U32 \
-    match ip dst $DST_IP \
-    match ip dport $DST_PORT 0xffff \
-    match ip sport $SRC_PORT 0xffff \
+    match ip src $DST_IP \
+    match ip sport $DST_PORT 0xffff \
+    match ip dport $SRC_PORT 0xffff \
     flowid 1:1
 
 }
